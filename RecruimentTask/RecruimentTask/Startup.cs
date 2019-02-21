0009962e1c124794f08f3db0fe61d59f7cbf2563
Pabl0 <Pawel1695@gmail.com>
2019-02-21 20:36:23 +0100

    Update Startup.cs

--- RecruimentTask/RecruimentTask/Startup.cs
+++ RecruimentTask/RecruimentTask/Startup.cs
@@ -28,7 +28,7 @@ namespace RecruimentTask
         {
             services.AddTransient<ProductContext>();
 
-            services.AddMvc().AddXmlDataContractSerializerFormatters();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
